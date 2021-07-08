f0512f658cbe895aabbd42c4ef485b0aad106306
Smith Scarborough <smithscarborough@gmail.com>
2021-07-07 22:27:37 -0500

    add refresh token and logout route

--- authServer.js
+++ authServer.js
@@ -7,8 +7,25 @@ const jwt = require('jsonwebtoken');
 // allow our app to use json from the body that gets passed up to it 
 app.use(express.json());
 
-// In a production app, you will probably want to store this in a better way than using let, since it will empty out every time - store in a database instead, for example
-let refreshToken = []
+// In a production app, you will probably want to store this in a database, since doing this will clear out the data in the array every time the server restarts.  But using this now for demo purposes:
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken: accessToken })
+    })
+})
+
+// allow deletion of refresh tokens
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
 
 app.use('/token', (req, res) => {
     const refreshToken = req.body.token
@@ -25,6 +42,7 @@ app.post('/login', (req, res) => {
 
     const accessToken = generateAccessToken(user)
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
     res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
